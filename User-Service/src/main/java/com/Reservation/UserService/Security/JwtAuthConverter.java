5d9db041214aa955e7ba024f211fdeaacb64935a
Benjiealcontin <Benjiealcontin23@gmail.com>
2023-11-01 18:03:43 +0800

    Add SecurityConfig

---
+++ User-Service/src/main/java/com/Reservation/UserService/Security/JwtAuthConverter.java
@@ -0,0 +1,90 @@
+package com.Reservation.UserService.Security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =
+            new JwtGrantedAuthoritiesConverter();
+
+    @Value("${jwt.auth.converter[0].resource-id}")
+    private String resourceId0;
+    @Value("${jwt.auth.converter[0].principle-attribute}")
+    private String principleAttribute0;
+
+    @Value("${jwt.auth.converter[1].resource-id}")
+    private String resourceId1;
+    @Value("${jwt.auth.converter[1].principle-attribute}")
+    private String principleAttribute1;
+
+    @Override
+    public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
+        Collection<GrantedAuthority> authorities = Stream.concat(
+                jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                extractResourceRoles(jwt).stream()
+        ).collect(Collectors.toSet());
+
+        return new JwtAuthenticationToken(
+                jwt,
+                authorities,
+                getPrincipleClaimName(jwt)
+        );
+    }
+
+    private String getPrincipleClaimName(Jwt jwt) {
+        String claimName = JwtClaimNames.SUB;
+        if (principleAttribute0 != null) {
+            claimName = principleAttribute0;
+        } else if (principleAttribute1 != null) {
+            claimName = principleAttribute1;
+        }
+        return jwt.getClaim(claimName);
+    }
+
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess;
+        Map<String, Object> resource;
+        Collection<String> resourceRoles;
+
+        if (jwt.getClaim("resource_access") == null) {
+            return Set.of();
+        }
+
+        resourceAccess = jwt.getClaim("resource_access");
+
+        if (resourceAccess.get(resourceId0) == null &&
+                resourceAccess.get(resourceId1) == null) {
+            return Set.of();
+        }
+
+        if (resourceAccess.get(resourceId0) != null) {
+            resource = (Map<String, Object>) resourceAccess.get(resourceId0);
+        } else {
+            resource = (Map<String, Object>) resourceAccess.get(resourceId1);
+        }
+
+        resourceRoles = (Collection<String>) resource.get("roles");
+        return resourceRoles
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
+    }
+
+}
---
+++ User-Service/src/main/java/com/Reservation/UserService/Security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.Reservation.UserService.Security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    public static final String ADMIN = "client_admin";
+    public static final String STUDENT = "client_student";
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests(auth ->
+                {
+                    auth.requestMatchers(HttpMethod.GET,
+                            "/actuator/**").permitAll();
+                    auth.requestMatchers(HttpMethod.GET,
+                            "api/reservationAllReservationByStatus/*",
+                            "api/reservation/AllReservation",
+                            "api/reservation/getReservation/*").hasRole(ADMIN);
+
+                    auth.anyRequest().authenticated();
+                });
+
+        http.
+                oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)
+                ));
+
+        http.
+                sessionManagement((session) ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
